33be1e96e83e054b5186dff51ed20a6d609f4c47
nxy <2659546337@qq.com>
2024-07-09 10:24:26 +0630

    vue format

--- src/preprocessors/vue-preprocessor.ts
+++ src/preprocessors/vue-preprocessor.ts
@@ -4,11 +4,32 @@ import { preprocessor } from './preprocessor';
 export function vuePreprocessor(code: string, options: PrettierOptions) {
     const { parse } = require('@vue/compiler-sfc');
     const { descriptor } = parse(code);
+    const scriptContent = descriptor.script?.content;
+    const scriptSetupContent = descriptor.scriptSetup?.content;
 
-    const content = (descriptor.script ?? descriptor.scriptSetup)?.content;
-    if (!content) {
+    if (!scriptContent && !scriptSetupContent) {
         return code;
     }
 
-    return code.replace(content, `\n${preprocessor(content, options)}\n`);
+    let transformedCode = code;
+
+    // https://github.com/trivago/prettier-plugin-sort-imports/pull/283
+    const replacer = (content: string) => {
+        // we pass the second argument as a function to avoid issues with the replacement string
+        // if string contained special groups (like $&, $`, $', $n, $<n>, etc.) this would produce invalid results
+        return transformedCode.replace(
+            content,
+            () => `\n${preprocessor(content, options)}\n`,
+        );
+    };
+
+    if (scriptContent) {
+        transformedCode = replacer(scriptContent);
+    }
+
+    if (scriptSetupContent) {
+        transformedCode = replacer(scriptSetupContent);
+    }
+
+    return transformedCode;
 }
